2a1f3302ff705a002c6fabb9dd714e89fadfa856
DorShireto <shireto.dor@gmail.com>
2021-05-22 16:12:09 +0300

    added to last commit

--- project/routes/utils/players_utils.js
+++ project/routes/utils/players_utils.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-const axios = require("axios");
-const api_domain = "https://soccer.sportmonks.com/api/v2.0";
-// const TEAM_ID = "85";
-
-async function getPlayerIdsByTeam(team_id) {
-  let player_ids_list = [];
-  const team = await axios.get(`${api_domain}/teams/${team_id}`, {
-    params: {
-      include: "squad",
-      api_token: process.env.api_token,
-    },
-  });
-  team.data.data.squad.data.map((player) =>
-    player_ids_list.push(player.player_id)
-  );
-  return player_ids_list;
-}
-
-async function getPlayersInfo(players_ids_list) {
-  let promises = [];
-  players_ids_list.map((id) =>
-    promises.push(
-      axios.get(`${api_domain}/players/${id}`, {
-        params: {
-          api_token: process.env.api_token,
-          include: "team",
-        },
-      })
-    )
-  );
-  let players_info = await Promise.all(promises);
-  return extractRelevantPlayerData(players_info);
-}
-
-function extractRelevantPlayerData(players_info) {
-  return players_info.map((player_info) => {
-    const { fullname, image_path, position_id } = player_info.data.data;
-    const { name } = player_info.data.data.team.data;
-    return {
-      name: fullname,
-      image: image_path,
-      position: position_id,
-      team_name: name,
-    };
-  });
-}
-
-async function getPlayersByTeam(team_id) {
-  let player_ids_list = await getPlayerIdsByTeam(team_id);
-  let players_info = await getPlayersInfo(player_ids_list);
-  return players_info;
-}
-
-exports.getPlayersByTeam = getPlayersByTeam;
-exports.getPlayersInfo = getPlayersInfo;
-=======
 const axios = require("axios");
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 // const TEAM_ID = "85";
@@ -99,6 +42,7 @@ function extractRelevantPlayerData(players_info) {
       position: position_id,
       team_name: name,
       //todo: add personal page here
+
     };
   });
 }
@@ -111,4 +55,3 @@ async function getPlayersByTeam(team_id) {
 
 exports.getPlayersByTeam = getPlayersByTeam;
 exports.getPlayersInfo = getPlayersInfo;
->>>>>>> 4266e9c92a8288da73b0e0dfabdc52c7868a5337
